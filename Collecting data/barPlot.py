ea24942d07f6808c8e4017da32a968e0a24c05a7
polo-0831 <B20040822@njupt.edu.cn>
2024-11-26 18:26:51 +0000

    update .csv

--- Collecting data/barPlot.py
+++ Collecting data/barPlot.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 Baseline_data = pd.read_csv(
     'https://github.com/polo-0831/Robotics-systems-Group/blob/master/Collecting%20data/Result_data_baseline.csv',
-    sep=',')
+    sep=',',
+    on_bad_lines='skip')
 Improved_data = pd.read_csv(
     'https://github.com/polo-0831/Robotics-systems-Group/blob/master/Collecting%20data/Result_data_improved.csv',
-    sep=',')
+    sep=',',
+    on_bad_lines='skip')
 
 # The following will produce a boxplot where each box collates
 # (collects together) the measurements for all angles, at the
@@ -20,6 +22,8 @@ Improved_data = pd.read_csv(
 #
 
 plt.figure(figsize=(12, 8))
+print(Baseline_data.columns)
+
 bplot = sns.boxplot(y='a', x='a0', data=Baseline_data, width=0.5)
 bplot.set_title('Measurements for Each Distance from Obstacle')
 bplot.set(xlabel="Distance (cm)", ylabel="Measurements (raw 8-bit ADC)")
